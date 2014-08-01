a7013eb04577412ae7a7cedfd0046e19c6baf2ef
Parkayun_add <parkayun@add2paper.com>
2014-08-01 15:12:02 +0900

    add search streams feature

--- libjaws/jaws.py
+++ libjaws/jaws.py
@@ -10,3 +10,12 @@ class Jaws(object):
 
     def search(self, q):
         return self.client.get('/tracks', q=q)
+
+    def search_streams(self, q):
+        result = []
+        for idx, track in enumerate(self.search(q)):
+            result.append({
+                'order': str(idx+1),
+                'uri': track.obj['uri']+'/stream/?client_id='+self.client.client_id
+            })
+        return result
