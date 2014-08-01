768941d56e6dde73b14cdf4314e639b673fc9e4d
Parkayun_add <parkayun@add2paper.com>
2014-08-01 15:59:14 +0900

    get meta data feature

--- libjaws/jaws.py
+++ libjaws/jaws.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
+import requests
 import soundcloud
+from bs4 import BeautifulSoup
 
 
 class Jaws(object):
@@ -8,14 +10,46 @@ class Jaws(object):
     def __init__(self, client_id=''):
         self.client = soundcloud.Client(client_id=client_id)
 
+    def get_track_meta_data(self, q):
+        results = []
+
+        url = 'http://www.melon.com/search/song/index.htm?q=%s&section=&searchGnbYn=Y&ipath=srch_form&ipath=srch_form' % q
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text)
+        track_table = soup.find('form', {'id': 'frm_defaultList'})
+        track_rows = track_table.findAll('tr')[1:]
+
+        for row in track_rows:
+            try:
+                data = row.findAll('a', {'class': 'fc_mgray'})
+                results.append({
+                    'title': row.find('a', {'class': 'fc_gray'}).text,
+                    'artist': data[0].text,
+                    'album': data[2].text
+                })
+            except (AttributeError, IndexError):
+                pass
+        return results
+
     def search(self, q):
-        return self.client.get('/tracks', q=q)
+        try:
+            return self.client.get('/tracks', q=q)[0]
+        except IndexError:
+            return None
+
+    def get_search_stream(self):
+        pass
 
     def search_streams(self, q):
         result = []
-        for idx, track in enumerate(self.search(q)):
-            result.append({
-                'order': str(idx+1),
-                'uri': track.obj['uri']+'/stream/?client_id='+self.client.client_id
-            })
+        tracks = self.get_track_meta_data(q)
+        for track in tracks:
+            data = self.search(track['title'] + ' ' + track['artist'])
+            if data is not None:
+                result.append({
+                    'title': track['title'],
+                    'artist': track['artist'],
+                    'album': track['album'],
+                    'uri': data.obj['uri']+'/stream/?client_id='+self.client.client_id
+                })
         return result
